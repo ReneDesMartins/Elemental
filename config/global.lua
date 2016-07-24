b2a1f6056888a317c1d31b1f73788e3a1bea5320
Patternsheep <meesrudigerard@live.nl>
2016-07-24 15:57:16 +0200

    Lib_Load and Mod_Load are now strings, where the libraries/modules are space-separated.

--- config/global.lua
+++ config/global.lua
@@ -1,14 +1,9 @@
 -- Global config goes here. For configuration details on specific modules, check the respective module file.
 return {
 	-- All the 'libraries' to be loaded. Supply (without extension) the libraries located in src/
-	lib_load={
-		"/src/function",
-		"/src/modules",
-		"/src/irc/irc",
-	},
+	lib_load="/src/function /src/modules /src/irc/irc",
 
 	-- All modules that have to be loaded before starting.
 	-- Just supply the name of the directory in mod/
-	mod_load={
-	},
+	mod_load="print log command_mod commands/eval commands/syn"
 }
